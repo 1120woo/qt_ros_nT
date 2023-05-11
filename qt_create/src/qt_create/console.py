a85f57640ff15788941ee960f04ac796c8195a27
C_Freesun <31441395+1120woo@users.noreply.github.com>
2023-05-11 22:31:07 +0900

    Update console.py

--- qt_create/src/qt_create/console.py
+++ qt_create/src/qt_create/console.py
@@ -1,17 +1,30 @@
-
 ##############################################################################
 # Console Output
 ##############################################################################
 
 class Console:
+    """
+    Class for formatting console output.
+    """
+
     bold = "\033[1m"
-    reset = "\033[0;0m"
+    reset = "\033[0m"
     red = "\033[31m"
 
-def red_string(msg):
-    """bound string with console symbols for red output"""
-    return Console.red + msg + Console.reset
+    def red_string(self, msg):
+        """
+        Formats the given string in red.
+        """
+        return self.red + msg + self.reset
 
-def bold_string(msg):
-    """bound string with console symbols for bold output"""
-    return Console.bold + msg + Console.reset
+    def bold_string(self, msg):
+        """
+        Formats the given string in bold.
+        """
+        return self.bold + msg + self.reset
+
+
+if __name__ == "__main__":
+    c = Console()
+    print(c.red_string("This is red text."))
+    print(c.bold_string("This is bold text."))
